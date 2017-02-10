e85e2b6625a94d7ed02cf8c40c2b0d63e9fab4d0
Phongxai Vang <phongxai.vang@gmail.com>
2017-02-10 15:36:35 -0600

    Third Update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-$("button").click(function() {
-    var bla = $('input:text').val();
+$(document).ready(function() {
+    $("button").click(function() {
+        var bla = $('input:text').val().trim();
 
 
-    if (bla.toLowerCase() === "student") {
-        $('.button a').attr('href', 'student.html');
-    }
+        if (bla.toLowerCase() === "student") {
+            $('.button a').attr('href', 'student.html');
+        }
 
-    if (bla.toLowerCase() === "teacher") {
-        $('.button a').attr('href', 'teacher.html');
-    }
+        if (bla.toLowerCase() === "teacher") {
+            $('.button a').attr('href', 'teacher.html');
+        }
+    });
+
+    $("#menu-toggle").click(function() {
+        $("#side-bar").toggle();
+    });
+
+    var resizeTimer;
+    $(window).on('resize', function(e) {
+        clearTimeout(resizeTimer);
+
+        if ($(window).width() > 500) {
+            $('#side-bar').show();
+        } else {
+            $('#side-bar').hide();
+        }
+    });
+
+    
 });
-
-// $(document).keypress(function(e) {
-//     if(e.which == 13) {
-//         $("button").click();
-//     }
-// });
