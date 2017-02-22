83a56ce8f9b2c7dc5d75e85eeeb3093a2a4e0375
Phongxai Vang <phongxai.vang@gmail.com>
2017-02-22 09:17:14 -0600

    Registration Update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-    $("button").click(function() {
-        var bla = $('input:text').val().trim();
-
-
-        if (bla.toLowerCase() === "student") {
-            $('.button a').attr('href', 'student.html');
-        }
-
-        if (bla.toLowerCase() === "teacher") {
-            $('.button a').attr('href', 'teacher.html');
-        }
-    });
+    loginClick();
+    registrationClick();
 
     $("#menu-toggle").click(function() {
         $("#side-bar").toggle();
     });
 
+    // Constant check of window resize
     var resizeTimer;
     $(window).on('resize', function(e) {
         clearTimeout(resizeTimer);
@@ -27,5 +18,33 @@ $(document).ready(function() {
         }
     });
 
-    
 });
+
+
+function loginClick() {
+  $("button").click(function() {
+      var bla = $('input:text').val().trim();
+
+      if (bla.toLowerCase() === "student") {
+          $('.button a').attr('href', 'student.html');
+      }
+
+      if (bla.toLowerCase() === "teacher") {
+          $('.button a').attr('href', 'teacher.html');
+      }
+  });
+}
+
+function registrationClick() {
+  $("#regis-button").click(function() {
+    var rEmail = $("#rEmail").val();
+
+    if(rEmail.indexOf("@") != -1) {
+      $("#regis-confirm").css("display", "inline-block");
+      $("#regis-form").hide();
+    }
+    else {
+      alert("invalid email")
+    }
+  })
+}
